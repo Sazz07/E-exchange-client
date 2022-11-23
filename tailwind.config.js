bb91412eca6d5f93f7b6657a8d026a2f0f8b4e47
Sazz07 <sazzad.hossain882@gmail.com>
2022-11-23 21:32:09 +0600

    Complete client side setup with tailwind react router dom and also done folder structure, added AuthContext

---
+++ tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{js,jsx,ts,tsx}",
+  ],
+  daisyui: {
+    themes: [
+      {
+        doctortheme: {
+          primary: '#2196f3',
+          secondary: '#ff216d',
+          accent: '#3A4256',
+          neutral: "#3D4451",
+          "base-100": "#FFFFFF",
+        }
+      }
+    ]
+  },
+  theme: {
+    extend: {},
+  },
+  plugins: [require("daisyui")],
+}
