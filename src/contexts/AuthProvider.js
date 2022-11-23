a410dbf770d0e294baf10f2a54352ea1c6b588ed
Sazz07 <sazzad.hossain882@gmail.com>
2022-11-23 23:15:23 +0600

    Navbar added

--- src/contexts/AuthProvider.js
+++ src/contexts/AuthProvider.js
@@ -1,15 +1,74 @@
-import React, { createContext } from 'react';
+import React, { createContext, useEffect, useState } from 'react';
+import { createUserWithEmailAndPassword, getAuth, GoogleAuthProvider, onAuthStateChanged, signInWithEmailAndPassword, signInWithPopup, signOut, updateProfile } from "firebase/auth";
+import app from '../firebase/firebase.config';
 
 export const AuthContext = createContext();
+const auth = getAuth(app);
 
-const authInfo = {
-    
-}
+const googleProvider = new GoogleAuthProvider();
+
 
 const AuthProvider = ({ children }) => {
+    const [user, setUser] = useState(null);
+    const [loading, setLoading] = useState(true);
+
+    //Create User With Email & Password
+    const createUser = (email, password) => {
+        setLoading(true)
+        return createUserWithEmailAndPassword(auth, email, password)
+    };
+
+    // Update user
+    const updateUser = (userInfo) => {
+        return updateProfile(auth.currentUser, userInfo);
+    };
+
+    //Login with Password
+    const signIn = (email, password) => {
+        setLoading(true)
+        return signInWithEmailAndPassword(auth, email, password)
+    };
+
+    // 4. Google Signin
+    const signInWithGoogle = () => {
+        setLoading(true);
+        return signInWithPopup(auth, googleProvider);
+    }
+
+    // 5. Logout
+    const logOut = () => {
+        setLoading(true);
+        return signOut(auth);
+    }
+
+
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, currentUser => {
+            console.log('User Observing', currentUser);
+            setUser(currentUser);
+            setLoading(false);
+        });
+
+        return () => {
+            unsubscribe()
+        };
+    }, [])
+
+
+
+    const authInfo = {
+        user,
+        createUser,
+        updateUser,
+        signIn,
+        signInWithGoogle,
+        logOut,
+        loading,
+        setLoading
+    }
     return (
         <AuthContext.Provider value={authInfo}>
-        {children}
+            {children}
         </AuthContext.Provider>
     );
 };
