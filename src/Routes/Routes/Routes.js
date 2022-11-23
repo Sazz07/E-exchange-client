a0bf9f74585e4bf013663071b09da1ec1fdf4ec0
Sazz07 <sazzad.hossain882@gmail.com>
2022-11-24 01:19:49 +0600

    404 page added

--- src/Routes/Routes/Routes.js
+++ src/Routes/Routes/Routes.js
@@ -2,12 +2,14 @@ import { createBrowserRouter } from "react-router-dom";
 import Main from "../../Layout/Main";
 import Home from "../../Pages/Home/Home/Home";
 import Login from "../../Pages/Login/Login";
+import ErrorElement from "../../Pages/Shared/ErrorElement/ErrorElement";
 import Signup from "../../Pages/Signup/Signup";
 
 const router = createBrowserRouter([
     {
         path: '/',
         element: <Main></Main>,
+        errorElement: <ErrorElement></ErrorElement>,
         children: [
             {
                 path: '/',
