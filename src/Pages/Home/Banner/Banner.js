440082ad6410caa4f3b31182174201acb8b865ce
Sazz07 <sazzad.hossain882@gmail.com>
2022-11-23 23:34:41 +0600

    Banner added

--- src/Pages/Home/Banner/Banner.js
+++ src/Pages/Home/Banner/Banner.js
@@ -1,10 +1,51 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
 const Banner = () => {
     return (
-        <div>
-            
-        </div>
+        <section
+            className="relative bg-[url(https://i.postimg.cc/NfyxM553/02sl-usedphone1-video-Sixteen-By-Nine-Jumbo1600.jpg)] bg-cover bg-center bg-no-repeat"
+        >
+            <div
+                className="absolute inset-0 bg-white/75 sm:bg-transparent sm:bg-gradient-to-r sm:from-white/75 sm:to-white/25"
+            ></div>
+
+            <div
+                className="relative mx-auto max-w-screen-xl px-4 py-32 sm:px-6 lg:flex lg:h-screen lg:items-center lg:px-8"
+            >
+                <div className="max-w-xl text-center sm:text-left">
+                    <h1 className="text-3xl font-extrabold sm:text-5xl">
+                        Let us find your
+
+                        <strong className="block font-extrabold text-rose-700">
+                            Forever Home.
+                        </strong>
+                    </h1>
+
+                    <p className="mt-4 max-w-lg sm:text-xl sm:leading-relaxed">
+                        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Nesciunt illo
+                        tenetur fuga ducimus numquam ea!
+                    </p>
+
+                    <div className="mt-8 flex flex-wrap gap-4 text-center">
+                        <Link
+                            
+                            className="block w-full rounded bg-rose-600 px-12 py-3 text-sm font-medium text-white shadow hover:bg-rose-700 focus:outline-none focus:ring active:bg-rose-500 sm:w-auto"
+                        >
+                            Get Started
+                        </Link>
+
+                        <Link
+                            
+                            className="block w-full rounded bg-white px-12 py-3 text-sm font-medium text-rose-600 shadow hover:text-rose-700 focus:outline-none focus:ring active:text-rose-500 sm:w-auto"
+                        >
+                            Learn More
+                        </Link>
+                    </div>
+                </div>
+            </div>
+        </section>
+
     );
 };
 
